358bec8b9fc2dc884fad231953c4ececaf0b1e56
birkin <birkin.diana@gmail.com>
2019-12-19 10:51:54 -0500

    queries people.

--- disa_app/views.py
+++ disa_app/views.py
@@ -49,16 +49,47 @@ def browse( request ):
         resp = render( request, 'disa_app_templates/browse.html', context )
     return resp
 
+
 def people( request ):
     log.debug( '\n\nstarting people()' )
     context = {}
 
 
 
-    # engine = sqlalchemy.create_engine( settings_app.DB_URL )
-    # log.debug( f'engine, ```{engine}```' )
-    # sqla_session = sqla_sessionmaker(bind = engine)
-    # log.debug( f'sqla_session, ```{sqla_session}```' )
+    # # ==========
+    # # works
+    # # ==========
+    # from sqlalchemy import Column, Integer, String, ForeignKey
+    # from sqlalchemy import create_engine
+    # engine = create_engine( settings_app.DB_URL, echo=True )
+    # from sqlalchemy.ext.declarative import declarative_base
+    # Base = declarative_base()
+
+    # class Citation(Base):
+    #     __tablename__ = '3_citations'
+    #     id = Column( Integer, primary_key=True )
+    #     citation_type_id = Column( Integer, ForeignKey('2_citation_types.id'), nullable=False )
+    #     display = Column( String(500) )
+    #     zotero_id = Column( String(255) )
+    #     # comments = Column( UnicodeText() )
+    #     acknowledgements = Column( String(255) )
+    #     # references = db.relationship('Reference', backref='citation', lazy=True)
+
+    #     def __repr__(self):
+    #         return f'<Citation {self.id}>'
+
+    # from sqlalchemy.orm import sessionmaker
+    # Session = sessionmaker(bind = engine)
+    # session = Session()
+    # resultset: list = session.query( Citation ).all()
+    # log.debug( f'type(resultset), `{type(resultset)}`' )
+
+    # for row in resultset:
+    #     citation: sqlalchemy.orm.state.InstanceState = row
+    #     # log.debug( f'type(row), `{type(row)}`' )
+    #     # log.debug( f'id, `{row.id}`; display, ```{row.display}```; citation_type_id, `{row.citation_type_id}`' )
+    #     log.debug( f'citation, ```{pprint.pformat(citation.__dict__)}```' )
+    #     # break
 
 
 
@@ -68,55 +99,45 @@ def people( request ):
     from sqlalchemy.ext.declarative import declarative_base
     Base = declarative_base()
 
-    class Citation(Base):
-        __tablename__ = '3_citations'
-        id = Column( Integer, primary_key=True )
-        citation_type_id = Column( Integer, ForeignKey('2_citation_types.id'), nullable=False )
-        display = Column( String(500) )
-        zotero_id = Column( String(255) )
-        # comments = Column( UnicodeText() )
-        acknowledgements = Column( String(255) )
-        # references = db.relationship('Reference', backref='citation', lazy=True)
+    class Person(Base):
+        __tablename__ = '1_people'
 
-        def __repr__(self):
-            return f'<Citation {self.id}>'
+        id = Column( Integer, primary_key=True )
+        first_name = Column( String(255) )
+        last_name = Column( String(255) )
+        comments = Column( String(255) )
+        # references = relationship( 'Referent', backref='person', lazy=True )
+
+        @classmethod
+        def filter_on_description( cls, desc ):
+            return cls.query.join(
+                cls.references).join(Referent.roles).filter(Role.name==desc)
+
+        def display_name( self ):
+            display = "{0} {1}".format(
+                self.first_name, self.last_name).strip()
+            if display == "":
+                return "Unknown"
+            else:
+                return display
+
+        def display_attr( self, attr ):
+            vals = { desc.name for ref in self.references
+                for desc in getattr(ref, attr) }
+            return ', '.join(list(vals))
 
     from sqlalchemy.orm import sessionmaker
     Session = sessionmaker(bind = engine)
     session = Session()
-    resultset: list = session.query( Citation ).all()
+    resultset: list = session.query( Person ).all()
     log.debug( f'type(resultset), `{type(resultset)}`' )
 
     for row in resultset:
-        citation: sqlalchemy.orm.state.InstanceState = row
+        person: sqlalchemy.orm.state.InstanceState = row
         # log.debug( f'type(row), `{type(row)}`' )
         # log.debug( f'id, `{row.id}`; display, ```{row.display}```; citation_type_id, `{row.citation_type_id}`' )
-        log.debug( f'citation, ```{pprint.pformat(citation.__dict__)}```' )
+        log.debug( f'person, ```{person.__dict__}```' )
         # break
-
-
-
-    # from sqlalchemy import Column, Integer, String
-    # from sqlalchemy import create_engine
-    # engine = create_engine('sqlite:///sales.db', echo = True)
-    # from sqlalchemy.ext.declarative import declarative_base
-    # Base = declarative_base()
-
-    # class Customers(Base):
-    #    __tablename__ = 'customers'
-    #    id = Column(Integer, primary_key =  True)
-    #    name = Column(String)
-
-    #    address = Column(String)
-    #    email = Column(String)
-
-    # from sqlalchemy.orm import sessionmaker
-    # Session = sessionmaker(bind = engine)
-    # session = Session()
-    # result = session.query(Customers).all()
-
-    # for row in result:
-    #    print ("Name: ",row.name, "Address:",row.address, "Email:",row.email)
 
 
 
@@ -141,6 +162,36 @@ def people( request ):
     #     people.append( prsn )
     # p = people[1]
     # log.debug( f'p.__dict__, ```{p.__dict__}```' )
+
+
+
+# class Person(db.Model):
+#     __tablename__ = '1_people'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.String(255))
+#     last_name = db.Column(db.String(255))
+#     comments = db.Column(db.String(255))
+#     references = db.relationship('Referent', backref='person', lazy=True)
+
+#     @classmethod
+#     def filter_on_description(cls, desc):
+#         return cls.query.join(
+#             cls.references).join(Referent.roles).filter(Role.name==desc)
+
+#     def display_name(self):
+#         display = "{0} {1}".format(
+#             self.first_name, self.last_name).strip()
+#         if display == "":
+#             return "Unknown"
+#         else:
+#             return display
+
+#     def display_attr(self, attr):
+#         vals = { desc.name for ref in self.references
+#             for desc in getattr(ref, attr) }
+#         return ', '.join(list(vals))
+
 
 
 
