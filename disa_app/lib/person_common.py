34bc4fbe5c9a3422f3426f04590dd79f91d1be78
birkin <birkin.diana@gmail.com>
2019-12-24 18:24:42 -0500

    starts person-view tribes/references work.

--- disa_app/lib/person_common.py
+++ disa_app/lib/person_common.py
@@ -21,7 +21,24 @@ log = logging.getLogger(__name__)
 
 
 def parse_person_name( prsn ) -> str:
+    log.debug( f'type(prsn), ```{type(prsn)}```' )
+    log.debug( f'prsn.__dict__, ```{prsn.__dict__}```' )
+    log.debug( f'prsn.references, ```{prsn.references}```' )
     name: str = f'{prsn.first_name} {prsn.last_name}'.strip()
     if name == '':
         name = 'Not Listed'
     return name
+
+
+def parse_person_descriptors( prsn, descriptor ):
+    vals = { desc.name for ref in prsn.references for desc in getattr(ref, descField) }
+    log.debug( f'vals, ```{vals}```' )
+    out = ', '.join(list(vals))
+    return out if out else 'None'
+
+
+# def parse_person_descriptors( prsn, descriptor ):
+#     vals = { desc.name for ref in personObj.references
+#                 for desc in getattr(ref, descField) }
+#     out = ', '.join(list(vals))
+#     return out if out else 'None'
