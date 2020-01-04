01dfa24d2f9aeecf3c4de89736441d0db7fd60db
birkin <birkin.diana@gmail.com>
2020-01-04 07:52:56 -0500

    updates env prefix.

--- disa_app/settings_app.py
+++ disa_app/settings_app.py
@@ -5,19 +5,19 @@ import json, os
 
 ## general
 
-README_URL = os.environ['DISA_DJ_19V1__README_URL']
+README_URL = os.environ['DISA_DJ__README_URL']
 
-DENORMALIZED_JSON_URL = os.environ['DISA_DJ_19V1__DENORMALIZED_JSON_URL']
+DENORMALIZED_JSON_URL = os.environ['DISA_DJ__DENORMALIZED_JSON_URL']
 
 
 ## db
 
-DB_URL = os.environ['DISA_DJ_19V1__DATABASE_URL']
+DB_URL = os.environ['DISA_DJ__DATABASE_URL']
 
 
 ## auth
-SUPER_USERS = json.loads( os.environ['DISA_DJ_19V1__SUPER_USERS_JSON'] )
-STAFF_USERS = json.loads( os.environ['DISA_DJ_19V1__STAFF_USERS_JSON'] )  # users permitted access to admin
-STAFF_GROUP = os.environ['DISA_DJ_19V1__STAFF_GROUP']  # not grouper-group; rather, name of django-admin group for permissions
-TEST_META_DCT = json.loads( os.environ['DISA_DJ_19V1__TEST_META_DCT_JSON'] )
-# POST_LOGIN_ADMIN_REVERSE_URL = os.environ['DISA_DJ_19V1__POST_LOGIN_ADMIN_REVERSE_URL']  # tricky; for a direct-view of a model, the string would be in the form of: `admin:APP-NAME_MODEL-NAME_changelist`
+SUPER_USERS = json.loads( os.environ['DISA_DJ__SUPER_USERS_JSON'] )
+STAFF_USERS = json.loads( os.environ['DISA_DJ__STAFF_USERS_JSON'] )  # users permitted access to admin
+STAFF_GROUP = os.environ['DISA_DJ__STAFF_GROUP']  # not grouper-group; rather, name of django-admin group for permissions
+TEST_META_DCT = json.loads( os.environ['DISA_DJ__TEST_META_DCT_JSON'] )
+# POST_LOGIN_ADMIN_REVERSE_URL = os.environ['DISA_DJ__POST_LOGIN_ADMIN_REVERSE_URL']  # tricky; for a direct-view of a model, the string would be in the form of: `admin:APP-NAME_MODEL-NAME_changelist`
