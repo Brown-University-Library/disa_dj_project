12181874e542ece09654cd0ea63d4417ffce25c7
birkin <birkin.diana@gmail.com>
2020-01-05 12:14:01 -0500

    skips shib-check if already logged-in.

--- disa_app/lib/shib_auth.py
+++ disa_app/lib/shib_auth.py
@@ -24,12 +24,17 @@ def shib_login(func):
         Called by views.py decorators. """
     log.debug( 'starting shib_login() decorator' )
     def decorator(request, *args, **kwargs):
-        hlpr = LoginDecoratorHelper()
-        cleaned_meta_dct = hlpr.prep_shib_dct( request.META, request.get_host() )
-        user_obj = hlpr.manage_usr_obj( request, cleaned_meta_dct )
-        if not user_obj:
-            # return HttpResponseForbidden( '403 / Forbidden' )
-            return HttpResponseRedirect( reverse('login_url') )
+        if request.user.is_authenticated == True:
+            log.debug( 'user already logged in; skip authentication' )
+            pass
+        else:
+            log.debug( 'user not logged in; proceed w/shib-check' )
+            hlpr = LoginDecoratorHelper()
+            cleaned_meta_dct = hlpr.prep_shib_dct( request.META, request.get_host() )
+            user_obj = hlpr.manage_usr_obj( request, cleaned_meta_dct )
+            if not user_obj:
+                # return HttpResponseForbidden( '403 / Forbidden' )
+                return HttpResponseRedirect( reverse('login_url') )
         return func(request, *args, **kwargs)
     return decorator
 
