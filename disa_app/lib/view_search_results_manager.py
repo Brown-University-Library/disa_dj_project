8376437ac3641e3372e2b8e09a5e4e79f8e94aa7
birkin <birkin.diana@gmail.com>
2020-02-05 15:37:43 -0500

    enhances roles and relationships display.

--- disa_app/lib/view_search_results_manager.py
+++ disa_app/lib/view_search_results_manager.py
@@ -51,17 +51,18 @@ def search_people( srch_text, session ):
             ) ).all()
     for person in qset_people:
         prsn_dct = person.dictify()
-        rfrncs = person.references
-        log.debug( f'type(rfrncs), ```{type(rfrncs)}```' )
+        if not prsn_dct['comments']:
+            prsn_dct['comments'] = '(None)'
+        rfrnts: List[models_alch.Referent] = person.references  # strange-but-true: this yields Referent records
         enslavements = []
         roles = []
-        for rfrnc in rfrncs:
-            rfrnc_enslavements = [ e.name for e in rfrnc.enslavements ]
-            if rfrnc_enslavements:
-                enslavements = list( set(enslavements + rfrnc_enslavements) )
-            rfrnc_roles = [ r.name for r in rfrnc.roles ]
-            if rfrnc_roles:
-                roles = list( set(roles + rfrnc_roles) )
+        for rfrnt in rfrnts:
+            rfrnt_enslavements = [ e.name for e in rfrnt.enslavements ]
+            if rfrnt_enslavements:
+                enslavements = list( set(enslavements + rfrnt_enslavements) )
+            rfrnt_roles = [ r.name for r in rfrnt.roles ]
+            if rfrnt_roles:
+                roles = list( set(roles + rfrnt_roles) )
         prsn_dct['enslavements'] = enslavements
         prsn_dct['roles'] = roles
         people.append( prsn_dct )
@@ -69,6 +70,38 @@ def search_people( srch_text, session ):
         'count': len(people), 'people': people, 'fields_searched': ['first_name', 'last_name', 'comments'] }
     log.debug( f'people_info, ```{pprint.pformat( people_info )}```' )
     return people_info
+
+
+# def search_people( srch_text, session ):
+#     """ Searches `Person` table.
+#         Called by run_search() """
+#     people = []
+#     qset_people = session.query( models_alch.Person ).filter(
+#         or_(
+#             models_alch.Person.first_name.contains( srch_text ),
+#             models_alch.Person.last_name.contains( srch_text ),
+#             models_alch.Person.comments.contains( srch_text )
+#             ) ).all()
+#     for person in qset_people:
+#         prsn_dct = person.dictify()
+#         rfrncs = person.references
+#         log.debug( f'type(rfrncs), ```{type(rfrncs)}```' )
+#         enslavements = []
+#         roles = []
+#         for rfrnc in rfrncs:
+#             rfrnc_enslavements = [ e.name for e in rfrnc.enslavements ]
+#             if rfrnc_enslavements:
+#                 enslavements = list( set(enslavements + rfrnc_enslavements) )
+#             rfrnc_roles = [ r.name for r in rfrnc.roles ]
+#             if rfrnc_roles:
+#                 roles = list( set(roles + rfrnc_roles) )
+#         prsn_dct['enslavements'] = enslavements
+#         prsn_dct['roles'] = roles
+#         people.append( prsn_dct )
+#     people_info = {
+#         'count': len(people), 'people': people, 'fields_searched': ['first_name', 'last_name', 'comments'] }
+#     log.debug( f'people_info, ```{pprint.pformat( people_info )}```' )
+#     return people_info
 
 
 # def search_people( srch_text, session ):
@@ -99,11 +132,30 @@ def search_citations( srch_text, session ):
             models_alch.Citation.comments.contains( srch_text )
             ) ).all()
     for cite in qset_citations:
-        citations.append( cite.dictify() )
+        cite_dct = cite.dictify()
+        if not cite_dct['comments']:
+            cite_dct['comments'] = '(None)'
+        citations.append( cite_dct )
     citations_info = {
         'count': len(citations), 'citations': citations, 'fields_searched': ['display', 'comments'] }
     log.debug( f'citations_info, ```{pprint.pformat( citations_info )}```' )
     return citations_info
+
+# def search_citations( srch_text, session ):
+#     """ Searches `Citation` table.
+#         Called by run_search() """
+#     citations = []
+#     qset_citations = session.query( models_alch.Citation ).filter(
+#         or_(
+#             models_alch.Citation.display.contains( srch_text ),
+#             models_alch.Citation.comments.contains( srch_text )
+#             ) ).all()
+#     for cite in qset_citations:
+#         citations.append( cite.dictify() )
+#     citations_info = {
+#         'count': len(citations), 'citations': citations, 'fields_searched': ['display', 'comments'] }
+#     log.debug( f'citations_info, ```{pprint.pformat( citations_info )}```' )
+#     return citations_info
 
 
 def search_items( srch_text, session ):
@@ -151,6 +203,3 @@ def experiment():
     print( '\n\n---\n\nItems...' )
     for x in qset_items:
         print( f'\n\n---\n\n```{pprint.pformat(x.__dict__)}```' )
-
-
-# experiment()
