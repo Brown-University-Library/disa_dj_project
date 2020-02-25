ceff232143f909b61504ecf80934425a6601c151
birkin <birkin.diana@gmail.com>
2020-02-25 15:50:05 -0500

    matches any existing user-profiles on user-creation.

--- disa_app/models.py
+++ disa_app/models.py
@@ -21,8 +21,8 @@ class UserProfile( models.Model ):
       `users = User.objects.all().select_related('profile')`
       ...to minimize extra db queries.
     """
-    # user = models.OneToOneField( User, on_delete=models.CASCADE, related_name='profile' )
-    user = models.OneToOneField( User, on_delete=models.CASCADE, related_name='profile', null=True, blank=True )  # null=True so I can pre-create the user-profile entries if desired
+    # user = models.OneToOneField( User, on_delete=models.CASCADE, related_name='profile', null=True, blank=True )  # null=True so I can pre-create the user-profile entries if desired
+    user = models.OneToOneField( User, on_delete=models.SET_NULL, related_name='profile', null=True, blank=True )  # null=True so I can pre-create the user-profile entries if desired
     uu_id = models.UUIDField( default=uuid.uuid4, editable=False )
     email = models.EmailField( default='', blank=True )
     old_db_id = models.IntegerField( null=True, blank=True )
@@ -31,15 +31,32 @@ class UserProfile( models.Model ):
 
 ## auto create and save UserProfile entries
 
+# @receiver( post_save, sender=User )
+# def create_user_profile(sender, instance, created, **kwargs):
+#     log.debug( f'starting create_user_profile(); created, ```{created}```; kwargs, ```{kwargs}```' )
+#     if created:  # meaning a User object was created
+#         log.debug( '`created` was True' )
+#         UserProfile.objects.create( user=instance )
+
 @receiver( post_save, sender=User )
 def create_user_profile(sender, instance, created, **kwargs):
     log.debug( f'starting create_user_profile(); created, ```{created}```; kwargs, ```{kwargs}```' )
-    if created:
+    if created:  # meaning a User object was created
         log.debug( '`created` was True' )
-        UserProfile.objects.create( user=instance )
+        log.debug( f'instance, ```{pprint.pformat(instance.__dict__)}```' )
+        try:  # see if there is an existing user-profile to associate -- this allows for user-profiles to be pre-created, and associated with the correct user-id when the user first logs in.
+            prfl = UserProfile.objects.get( email=instance.email )
+            if prfl.user != instance:
+                prfl.user = instance
+                prfl.save()
+                log.debug( 'pre-existing user-profile has been matched to user' )
+        except:
+            log.exception( 'user has no matching user-profile, so one will be created (traceback follows; processing will continue)' )
+            UserProfile.objects.create( user=instance )
 
 @receiver( post_save, sender=User )
 def save_user_profile(sender, instance, **kwargs):
     log.debug( f'starting save_user_profile(); kwargs, ```{kwargs}```' )
+    log.debug( f'instance, ```{pprint.pformat(instance.__dict__)}```' )
     instance.profile.email = instance.email  # in case preferred shib email is updated.
     instance.profile.save()
