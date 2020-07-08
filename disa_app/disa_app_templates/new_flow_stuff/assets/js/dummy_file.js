367dcb49a609e5647a3390df2e0c58f14220db49
Patrick Rashleigh <edward_rashleigh@brown.edu>
2020-07-08 17:04:41 -0400

    Added "specify age in years" toggle behavior

--- disa_app/disa_app_templates/new_flow_stuff/assets/js/dummy_file.js
+++ disa_app/disa_app_templates/new_flow_stuff/assets/js/dummy_file.js
@@ -1,1 +1,11 @@
-placeholder
+
+
+const AGE_BY_NUMBER_CHECKBOX_ID = 'formInputSpecifiedByNumber',
+      AGE_BY_NUMBER_DISPLAY_CSS_CLASS = 'age-as-number';
+
+document.getElementById(AGE_BY_NUMBER_CHECKBOX_ID)
+        .onchange = function(e) {
+          const classOp = this.checked ? 'add' : 'remove';
+          document.body.classList[classOp](AGE_BY_NUMBER_DISPLAY_CSS_CLASS);
+        };
+
