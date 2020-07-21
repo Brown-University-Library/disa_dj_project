a9fa170668bb9a2c634c290aeb9fdd395835ba18
birkin <birkin.diana@gmail.com>
2020-07-21 12:50:23 -0400

    implements redesign static-files.

---
+++ disa_app/static/js/redesign_dummy_file.js
@@ -0,0 +1,11 @@
+
+
+const AGE_BY_NUMBER_CHECKBOX_ID = 'formInputSpecifiedByNumber',
+      AGE_BY_NUMBER_DISPLAY_CSS_CLASS = 'age-as-number';
+
+document.getElementById(AGE_BY_NUMBER_CHECKBOX_ID)
+        .onchange = function(e) {
+          const classOp = this.checked ? 'add' : 'remove';
+          document.body.classList[classOp](AGE_BY_NUMBER_DISPLAY_CSS_CLASS);
+        };
+
