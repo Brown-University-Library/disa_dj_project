c18e5cd6b7d670f0d7bc41a88de479701384fa1e
birkin <birkin.diana@gmail.com>
2020-10-14 10:58:01 -0400

    moves filter code into class.

--- disa_app/lib/temp_explore.py
+++ disa_app/lib/temp_explore.py
@@ -32,28 +32,18 @@ log.info( '\n\ndenormalizer log ready' )
 def main():
 
     session = make_session()
+    filter_deleted = FilterDeleted()
 
-    deleted_referent_ids = get_deleted_referent_ids()
+    deleted_referent_ids = filter_deleted.get_deleted_referent_ids()
 
-    deleted_referent_objs = get_deleted_referent_objs( deleted_referent_ids, session )
+    deleted_referent_objs = filter_deleted.get_deleted_referent_objs( deleted_referent_ids, session )
 
-    persons_to_ignore = get_persons_to_ignore( deleted_referent_objs, session )
+    persons_to_ignore = filter_deleted.get_persons_to_ignore( deleted_referent_objs, session )
 
     persons = session.query( models_alch.Person ).all()
     log.debug( f'persons count, ``{len(persons)}``' )
 
-    filtered_persons = []
-    for person in persons:
-        person_check = 'init'
-        for person_to_ignore in persons_to_ignore:
-            if person.id == person_to_ignore.id:
-                person_check = 'found'
-                break
-        if person_check == 'init':
-            filtered_persons.append( person )
-        else:
-            log.debug( f'filtering out person, ``{person}``' )
-            person_check = 'init'
+    filtered_persons = filter_deleted.apply_filter( persons, persons_to_ignore, session )
     log.debug( f'filtered_persons count, ``{len(filtered_persons)}``' )
 
     ## end def main()
@@ -64,49 +54,67 @@ def main():
 # -------
 
 
-def get_persons_to_ignore( deleted_referent_objs, session ):
-    persons = []
-    for deleted_referent_obj in deleted_referent_objs:
-        person_obj = session.query( models_alch.Person ).filter_by( id=deleted_referent_obj.person_id ).first()
-        log.debug( f'for referent ``{deleted_referent_obj}``, person is, ``{person_obj}``' )
-        persons.append( person_obj )
-    log.debug( f'persons to ignore, ``{persons}``' )
-    return persons
-
-
-
-def get_deleted_referent_objs( deleted_referent_ids, session ):
-    objs = []
-    for referent_id in deleted_referent_ids:
-        referent_obj = session.query( models_alch.Referent ).filter_by( id=referent_id ).first()
-        objs.append( referent_obj )
-    log.debug( f'referent objs, ``{objs}``' )
-    return objs
-
-
-def get_deleted_referent_ids():
-    referent_ids = []
-    marked_entries = MarkedForDeletion.objects.all()
-    for entry in marked_entries:
-        doc = json.loads( entry.doc_json_data )
-        references = doc.get( 'references', None )
-        if references:
-            for reference in references:
-                referents = reference.get( 'referents', None )
-                if referents:
-                    for referent in referents:
-                        referent_ids.append( referent['id'] )
-    sorted_referent_ids = sorted( referent_ids )
-    log.debug( f'referent_ids, ``{sorted_referent_ids}``' )
-    return sorted_referent_ids
-    # return [ 1819, 1820 ]
-
-
 def make_session() -> sqlalchemy.orm.session.Session:
     engine = create_engine( settings_app.DB_URL, echo=False )
     Session = sessionmaker( bind=engine )
     session = Session()
     return session
+
+
+class FilterDeleted():
+
+    def __init__( self ):
+        pass
+
+    def apply_filter( self, persons, persons_to_ignore, session ):
+        filtered_persons = []
+        for person in persons:
+            person_check = 'init'
+            for person_to_ignore in persons_to_ignore:
+                if person.id == person_to_ignore.id:
+                    person_check = 'found'
+                    break
+            if person_check == 'init':
+                filtered_persons.append( person )
+            else:
+                log.debug( f'filtering out person, ``{person}``' )
+                person_check = 'init'
+        return filtered_persons
+
+    def get_persons_to_ignore( self, deleted_referent_objs, session ):
+        persons = []
+        for deleted_referent_obj in deleted_referent_objs:
+            person_obj = session.query( models_alch.Person ).filter_by( id=deleted_referent_obj.person_id ).first()
+            log.debug( f'for referent ``{deleted_referent_obj}``, person is, ``{person_obj}``' )
+            persons.append( person_obj )
+        log.debug( f'persons to ignore, ``{persons}``' )
+        return persons
+
+    def get_deleted_referent_objs( self, deleted_referent_ids, session ):
+        objs = []
+        for referent_id in deleted_referent_ids:
+            referent_obj = session.query( models_alch.Referent ).filter_by( id=referent_id ).first()
+            objs.append( referent_obj )
+        log.debug( f'referent objs, ``{objs}``' )
+        return objs
+
+    def get_deleted_referent_ids( self ):
+        referent_ids = []
+        marked_entries = MarkedForDeletion.objects.all()
+        for entry in marked_entries:
+            doc = json.loads( entry.doc_json_data )
+            references = doc.get( 'references', None )
+            if references:
+                for reference in references:
+                    referents = reference.get( 'referents', None )
+                    if referents:
+                        for referent in referents:
+                            referent_ids.append( referent['id'] )
+        sorted_referent_ids = sorted( referent_ids )
+        log.debug( f'referent_ids, ``{sorted_referent_ids}``' )
+        return sorted_referent_ids
+
+    ## end class FilterDeleted()
 
 
 
