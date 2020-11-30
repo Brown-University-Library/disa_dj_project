6ca8e03cc3114a81c8a2bb160a4e310afc752e8e
Patrick Rashleigh <edward_rashleigh@brown.edu>
2020-11-30 13:29:55 -0500

    Handle scenario in which "age by number" checkbox doesn't exist

--- disa_app/static/js/redesign_dummy_file.js
+++ disa_app/static/js/redesign_dummy_file.js
@@ -3,9 +3,13 @@
 const AGE_BY_NUMBER_CHECKBOX_ID = 'formInputSpecifiedByNumber',
       AGE_BY_NUMBER_DISPLAY_CSS_CLASS = 'age-as-number';
 
-document.getElementById(AGE_BY_NUMBER_CHECKBOX_ID)
-        .onchange = function(e) {
-          const classOp = this.checked ? 'add' : 'remove';
-          document.body.classList[classOp](AGE_BY_NUMBER_DISPLAY_CSS_CLASS);
-        };
+const ageByNumberCheckbox = document.getElementById(AGE_BY_NUMBER_CHECKBOX_ID);
+
+if (ageByNumberCheckbox) {
+  ageByNumberCheckbox.onchange = function(e) {
+    const classOp = this.checked ? 'add' : 'remove';
+    document.body.classList[classOp](AGE_BY_NUMBER_DISPLAY_CSS_CLASS);
+  };
+}
+
 
