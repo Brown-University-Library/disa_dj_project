4e0987ed8a7864dfb3d545eda85a5b22f314b42a
birkin <birkin.diana@gmail.com>
2021-01-26 16:57:46 -0500

    sets setting from envar.

--- disa_app/settings_app.py
+++ disa_app/settings_app.py
@@ -19,6 +19,7 @@ DB_URL = os.environ['DISA_DJ__DATABASE_URL']
 
 
 ## auth
+
 SUPER_USERS = json.loads( os.environ['DISA_DJ__SUPER_USERS_JSON'] )
 STAFF_USERS = json.loads( os.environ['DISA_DJ__STAFF_USERS_JSON'] )  # users permitted access to admin
 STAFF_GROUP = os.environ['DISA_DJ__STAFF_GROUP']  # not grouper-group; rather, name of django-admin group for permissions
@@ -26,3 +27,8 @@ TEST_META_DCT = json.loads( os.environ['DISA_DJ__TEST_META_DCT_JSON'] )
 # POST_LOGIN_ADMIN_REVERSE_URL = os.environ['DISA_DJ__POST_LOGIN_ADMIN_REVERSE_URL']  # note, for a direct-view of a django-db-model, the string would be in the form of: `admin:APP-NAME_MODEL-NAME_changelist`
 
 LOGIN_PROBLEM_EMAIL = os.environ['DISA_DJ__LOGIN_PROBLEM_EMAIL']
+
+
+## basic-auth
+
+BASIC_AUTH_LIST = json.loads( os.environ['DISA_DJ__BASIC_AUTH_USERPASS_JSON'] )
