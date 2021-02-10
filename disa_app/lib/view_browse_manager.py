753f3bc1968a524bbb3972dc43704fc4d0f163ed
birkin <birkin.diana@gmail.com>
2021-02-10 12:13:27 -0500

    adds login logic.

--- disa_app/lib/view_browse_manager.py
+++ disa_app/lib/view_browse_manager.py
@@ -21,9 +21,12 @@ def check_browse_logged_in( session_items, is_logged_in_via_django ):
     log.debug( f'type(session_items), ``{type(session_items)}``' )
     log.debug( f'is_logged_in_via_django start, ``{is_logged_in_via_django}``' )
     log.debug( f'type(is_logged_in_via_django), ``{type(is_logged_in_via_django)}``' )
-    assert type( session_items ) == type( {}.items() )
+    # assert type( session_items ) == type( {}.items() )
+    assert type( session_items ) == dict
     assert type( is_logged_in_via_django ) == bool
     is_logged_in = False
     if is_logged_in_via_django:
+        is_logged_in = True
+    elif session_items.get( 'browse_logged_in', "no" ) == "yes":
         is_logged_in = True
     return is_logged_in
