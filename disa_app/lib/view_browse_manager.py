841c53a2d4fa3f002a511b11ff648b0519cac1ae
birkin <birkin.diana@gmail.com>
2021-02-10 08:43:02 -0500

    adds browse-login-check helper.

---
+++ disa_app/lib/view_browse_manager.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import logging, pprint
+
+# import sqlalchemy
+# from disa_app import models_sqlalchemy as models_alch
+# from disa_app import settings_app
+# from disa_app.lib import person_common
+# from django.conf import settings
+# from django.core.urlresolvers import reverse
+# from django.http import HttpResponse
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
+
+log = logging.getLogger(__name__)
+
+
+def check_browse_logged_in( session_items, is_logged_in_via_django ):
+    log.debug( f'session_items start, ``{pprint.pformat(session_items)}``' )
+    log.debug( f'type(session_items), ``{type(session_items)}``' )
+    log.debug( f'is_logged_in_via_django start, ``{is_logged_in_via_django}``' )
+    log.debug( f'type(is_logged_in_via_django), ``{type(is_logged_in_via_django)}``' )
+    assert type( session_items ) == type( {}.items() )
+    assert type( is_logged_in_via_django ) == bool
+    is_logged_in = False
+    if is_logged_in_via_django:
+        is_logged_in = True
+    return is_logged_in
