d7e412badc9a1cf6e0505118a309173f65354dcf
birkin <birkin.diana@gmail.com>
2021-03-03 04:22:55 -0500

    fully implements updater.

--- disa_app/lib/view_data_group_manager.py
+++ disa_app/lib/view_data_group_manager.py
@@ -21,6 +21,130 @@ def make_session() -> sqlalchemy.orm.session.Session:
     return session
 
 
+class Updater():
+
+    def __init__( self, request_url, start_time ):
+        """ Updated by manage_put() """
+        assert type(request_url) == str
+        assert type(start_time) == datetime.datetime, type(start_time)
+        self.session = None
+        self.common = Common( request_url, start_time )
+        self.grp = None
+        self.prelim_status_code = None
+        self.perceived_count = None
+        self.perceived_count_estimated = None
+        self.perceived_description = None
+        self.perceived_reference_id = None
+
+    def validate_put_params( self, request_body ) -> bool:
+        """ Checks put params.
+            Called by views.data_reference_group(), triggered by views.edit_record() webpage. """
+        log.debug( 'starting validate_put_params()' )
+        assert type(request_body) == bytes
+        log.debug( f'request_body, ``{request_body}``' )
+        validity = False
+        put_dct = json.loads( request_body )
+        assert type(put_dct) == dict
+        try:
+            assert type(put_dct) == dict
+            assert type( put_dct['count'] ) == int
+            assert type( put_dct['count_estimated'] ) == bool
+            assert type( put_dct['description'] ) == str
+            assert type( put_dct['reference_id'] ) == int
+            self.perceived_count = put_dct['count']
+            self.perceived_count_estimated = put_dct['count_estimated']
+            self.perceived_description = put_dct['description']
+            self.perceived_reference_id = put_dct['reference_id']
+            validity = True
+        except:
+            log.exception( 'bad params; traceback follows; processing will continue' )
+        log.debug( f'validity, ``{validity}``' )
+        return validity
+
+    def manage_put( self, incoming_group_uuid, user_id ) -> HttpResponse:
+        """ Manages data/reference_group api ajax 'PUT'.
+            Called by views.data_reference_group(), triggered by views.edit_record() webpage. """
+        log.debug( 'starting manage_put' )
+        assert type(incoming_group_uuid) == str
+        assert type(user_id) == int
+        self.session = make_session()
+        try:
+            self.execute_put_get_grp_obj( incoming_group_uuid )
+            self.execute_put_save( user_id )
+            resp_dct = self.prep_put_response_data()  # self.grp contains data
+            resp = HttpResponse( json.dumps(resp_dct, sort_keys=True, indent=2), content_type='application/json; charset=utf-8' )
+        except:
+            msg = 'problem with save, or with response-prep; see logs'
+            log.exception( msg )
+            resp = HttpResponse( msg, status=500 )
+        log.debug( 'returning response' )
+        return resp
+
+    def execute_put_get_grp_obj( self, incoming_group_uuid ):
+        """ Gets grp-obj and stores it to attribute.
+            Called by manage_put() """
+        assert type(incoming_group_uuid) == str
+        try:
+            grp = self.session.query( models_alch.Group ).get( incoming_group_uuid )
+            assert type(grp) == models_alch.Group or isinstance(grp, type(None))
+            if grp:
+                log.debug( 'group found' )
+                self.grp = grp
+            else:
+                raise Exception( 'group not found' )
+        except:
+            log.exception( 'problem querying db; traceback follows; processing will halt' )
+            raise Exception( 'problem querying db' )
+        return
+
+    def execute_put_save( self, user_id ) -> dict:
+        """ Updates db and returns data.
+            Called by manage_put() """
+        log.debug( 'starting Poster.execute_save()' )
+        assert type(user_id) == int
+        try:
+            ## grp.uuid already set
+            self.grp.count = self.perceived_count
+            self.grp.count_estimated = self.perceived_count_estimated
+            self.grp.description = self.perceived_description
+            self.grp.reference_id = self.perceived_reference_id
+            ## grp.date_created already set
+            self.grp.date_modified = datetime.datetime.now()
+            self.session.add( self.grp )
+            self.session.commit()  # returns None, so no way to verify success
+            self.common.stamp_edit( user_id, self.grp.reference, self.session )
+        except:
+            log.exception( 'problem with database-save; traceback follows; processing will halt' )
+            raise Exception( 'problem with database-save' )
+        log.debug( 'put-save complete' )
+        return
+
+    def prep_put_response_data( self ):
+        """ Prepares put-dict data for response.
+            Called by manage_put() """
+        log.debug( 'starting prep_put_response_data()' )
+        response_dct = self.common.prepare_common_response_dct( self.grp )
+        assert type(response_dct) == dict
+        return_dct = {
+            'request': {
+                'url': self.common.request_url,
+                'method': 'PUT',
+                'payload': {
+                    'count': self.perceived_count,
+                    'count_estimated': self.perceived_count_estimated,
+                    'description': self.perceived_description,
+                    'reference_id': self.perceived_reference_id
+                    },
+                'timestamp': str( self.common.start_time )
+            },
+            'response': response_dct
+        }
+        log.debug( f'return_dct, ``{pprint.pformat(return_dct)}``' )
+        return return_dct
+
+    ## end class Updater()
+
+
 class Deleter():
 
     def __init__( self, request_url, start_time ):
@@ -91,10 +215,6 @@ class Deleter():
     ## end class Delete()
 
 
-
-
-
-
 class Poster():
 
     def __init__( self, request_url, start_time ):
@@ -108,26 +228,35 @@ class Poster():
         self.perceived_description = None
         self.perceived_reference_id = None
 
-    def validate_post_params( self, post_dict ) -> bool:
-        """ Checks post params.
-            Called by views.data_reference_group(), triggered by views.edit_record() webpage 'Add Group' button save. """
-        log.debug( 'starting Poster.validate_params()' )
-        validity = False
-        try:
-            assert type(post_dict) == dict, type(post_dict)
-            assert type( int(post_dict['count'][0]) ) == int
-            assert type( bool(post_dict['count_estimated'][0]) ) == bool
-            assert type( post_dict['description'][0] ) == str
-            assert type( int(post_dict['reference_id'][0]) ) == int
-            self.perceived_count = post_dict['count'][0]
-            self.perceived_count_estimated = post_dict['count_estimated'][0]
-            self.perceived_description = post_dict['description'][0]
-            self.perceived_reference_id = post_dict['reference_id'][0]
-            validity = True
-        except:
-            log.exception( 'bad params; traceback follows; processing will continue' )
+    def validate_post_params( self, post_dct ) -> bool:
+        """ Checks put params.
+            Called by views.data_reference_group(), triggered by views.edit_record() webpage. """
+        log.debug( 'starting validate_post_params()' )
+        assert type(post_dct) == dict
+        validity = self.common.validate_post_put_params( post_dct )
         log.debug( f'validity, ``{validity}``' )
         return validity
+
+    # def validate_post_params( self, post_dict ) -> bool:
+    #     """ Checks post params.
+    #         Called by views.data_reference_group(), triggered by views.edit_record() webpage 'Add Group' button save. """
+    #     log.debug( 'starting Poster.validate_params()' )
+    #     validity = False
+    #     try:
+    #         assert type(post_dict) == dict, type(post_dict)
+    #         assert type( int(post_dict['count'][0]) ) == int
+    #         assert type( bool(post_dict['count_estimated'][0]) ) == bool
+    #         assert type( post_dict['description'][0] ) == str
+    #         assert type( int(post_dict['reference_id'][0]) ) == int
+    #         self.perceived_count = post_dict['count'][0]
+    #         self.perceived_count_estimated = post_dict['count_estimated'][0]
+    #         self.perceived_description = post_dict['description'][0]
+    #         self.perceived_reference_id = post_dict['reference_id'][0]
+    #         validity = True
+    #     except:
+    #         log.exception( 'bad params; traceback follows; processing will continue' )
+    #     log.debug( f'validity, ``{validity}``' )
+    #     return validity
 
     def manage_post( self, post_dict, user_id ) -> HttpResponse:
         """ Manages data/reference_group api ajax 'POST'.
@@ -165,7 +294,7 @@ class Poster():
         assert type(uid) == str
         grp.uuid = uid
         grp.count = count
-        grp.count_estimated = True
+        grp.count_estimated = count_estimated
         grp.description = description
         grp.reference_id = reference_id
         grp.date_created = datetime.datetime.now()
@@ -177,9 +306,9 @@ class Poster():
         return grp.uuid
 
     def prep_post_response_data( self, uid ):
-        """ Prepares post-dict.
-            Called by manage_posst() """
-        log.debug( 'starting Poster.prep_post_response_data()' )
+        """ Prepares post-dict data for response.
+            Called by manage_post() """
+        log.debug( 'starting prep_post_response_data()' )
         assert type(uid) == str, type(uid)
         try:
             grp = self.session.query( models_alch.Group ).get( uid )
@@ -310,5 +439,25 @@ class Common():
         session.add( edit )
         session.commit()
         return
+
+    def validate_post_put_params( self, param_dct ):
+        """ Handles check for both the post and put validation step.
+            Called by manage_post() and manage_put() """
+        validity = False
+        try:
+            assert type(param_dct) == dict
+            assert type( int(param_dct['count'][0]) ) == int
+            assert type( bool(param_dct['count_estimated'][0]) ) == bool
+            assert type( param_dct['description'][0] ) == str
+            assert type( int(param_dct['reference_id'][0]) ) == int
+            self.perceived_count = param_dct['count'][0]
+            self.perceived_count_estimated = param_dct['count_estimated'][0]
+            self.perceived_description = param_dct['description'][0]
+            self.perceived_reference_id = param_dct['reference_id'][0]
+            validity = True
+        except:
+            log.exception( 'bad params; traceback follows; processing will continue' )
+        log.debug( f'validity, ``{validity}``' )
+        return validity
 
     ## end class Common()
