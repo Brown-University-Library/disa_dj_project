f8439a936f2bc1a405c95b2697b44632a6069da2
birkin <birkin.diana@gmail.com>
2021-03-04 14:07:01 -0500

    adds separator for log-viewing convenience.

--- disa_app/lib/shib_auth.py
+++ disa_app/lib/shib_auth.py
@@ -24,7 +24,7 @@ def shib_login(func):
         Called by views.py decorators. """
     # log.debug( 'starting shib_login() decorator' )
     def decorator(request, *args, **kwargs):
-        log.debug( f'coming from, ```{request.META.get("HTTP_REFERER", "referrer_unknown")}```' )
+        log.debug( f'\n\n starting decorator(); coming from, ```{request.META.get("HTTP_REFERER", "referrer_unknown")}```' )
         log.debug( f'heading to, ```{request.META["PATH_INFO"]}```' )
         log.debug( f'authenticated?, ```{request.user.is_authenticated}```' )
         log.debug( f'args, ```{args}```' )
