d1c204f9b10b0a6c4ec42d39572b0cc1ef782a74
birkin <birkin.diana@gmail.com>
2021-03-05 13:35:53 -0500

    updates put test to use known existing group.

--- disa_app/tests/test_data_group.py
+++ disa_app/tests/test_data_group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import json, logging, pprint, uuid
+import json, logging, pprint, secrets, uuid
 
 import requests
 from disa_app import settings_app
@@ -93,26 +93,17 @@ class Client_ReferenceGroup_Test( TestCase ):
 
     def test_put_good(self):
         """ Checks good PUT of `http://127.0.0.1:8000/data/reference_group/abcd/`. """
-        ## put -> create entry ==================
-        post_url = reverse( 'data_group_url', kwargs={'incoming_uuid': 'new'} )
-        log.debug( f'post-url, ``{post_url}``' )
-        post_payload = {
-            'count': 7,
-            'count_estimated': True,
-            'description': 'the description',
-            'reference_id': 49
-        }
-        post_response = self.client.post( post_url, post_payload )
-        post_resp_dct = json.loads( post_response.content )
-        new_uuid = post_resp_dct['response']['group_data']['uuid']
-        self.assertEqual( str, type(new_uuid) )
+        uuid = 'e2be12a88dc2455dbb0aaaab84828dc8'
+        self.assertEqual( str, type(uuid) )
         ## put -> put ===========================
-        put_url = reverse( 'data_group_url', kwargs={'incoming_uuid': new_uuid} )
+        put_url = reverse( 'data_group_url', kwargs={'incoming_uuid': uuid} )
         log.debug( f'put_url-url, ``{put_url}``' )
+        possible_counts = [ 2, 4, 6, 8, 10 ]
+        possible_descriptions = [ 'fooA', 'fooB', 'fooC', 'fooD', 'fooE' ]
         put_payload = {
-            'count': 8,
+            'count': secrets.choice( possible_counts ),
             'count_estimated': False,
-            'description': 'the new description',
+            'description': secrets.choice( possible_descriptions ),
             'reference_id': 49
         }
         jsn = json.dumps( put_payload )
@@ -129,16 +120,55 @@ class Client_ReferenceGroup_Test( TestCase ):
         self.assertEqual( ['elapsed_time', 'group_data'], resp_keys )
         resp_group_data_keys = sorted( resp_dct['response']['group_data'].keys() )
         self.assertEqual( ['count', 'count_estimated', 'date_created', 'date_modified', 'description', 'reference_id', 'uuid' ], resp_group_data_keys )
-        ## put -> deletion ======================
-        delete_url = reverse( 'data_group_url', kwargs={'incoming_uuid': new_uuid} )
-        delete_response = self.client.delete( delete_url )
-        self.assertEqual( 200, delete_response.status_code )
-        resp_dct = json.loads( delete_response.content )
-        self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
-        req_keys = sorted( resp_dct['request'].keys() )
-        self.assertEqual( ['method', 'payload', 'timestamp', 'url'], req_keys )
-        resp_keys = sorted( resp_dct['response'].keys() )
-        self.assertEqual( ['elapsed_time', 'status'], resp_keys )
+
+    # def test_put_good(self):
+    #     """ Checks good PUT of `http://127.0.0.1:8000/data/reference_group/abcd/`. """
+    #     ## put -> create entry ==================
+    #     post_url = reverse( 'data_group_url', kwargs={'incoming_uuid': 'new'} )
+    #     log.debug( f'post-url, ``{post_url}``' )
+    #     post_payload = {
+    #         'count': 7,
+    #         'count_estimated': True,
+    #         'description': 'the description',
+    #         'reference_id': 49
+    #     }
+    #     post_response = self.client.post( post_url, post_payload )
+    #     post_resp_dct = json.loads( post_response.content )
+    #     new_uuid = post_resp_dct['response']['group_data']['uuid']
+    #     self.assertEqual( str, type(new_uuid) )
+    #     ## put -> put ===========================
+    #     put_url = reverse( 'data_group_url', kwargs={'incoming_uuid': new_uuid} )
+    #     log.debug( f'put_url-url, ``{put_url}``' )
+    #     put_payload = {
+    #         'count': 8,
+    #         'count_estimated': False,
+    #         'description': 'the new description',
+    #         'reference_id': 49
+    #     }
+    #     jsn = json.dumps( put_payload )
+    #     put_response = self.client.put( put_url, data=jsn )
+    #     put_resp_dct = json.loads( put_response.content )
+    #     self.assertEqual( 200, put_response.status_code )
+    #     resp_dct = json.loads( put_response.content )
+    #     self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
+    #     req_keys = sorted( resp_dct['request'].keys() )
+    #     self.assertEqual( ['method', 'payload', 'timestamp', 'url'], req_keys )
+    #     req_payload_keys = sorted( resp_dct['request']['payload'].keys() )
+    #     self.assertEqual( ['count', 'count_estimated', 'description', 'reference_id'], req_payload_keys )
+    #     resp_keys = sorted( resp_dct['response'].keys() )
+    #     self.assertEqual( ['elapsed_time', 'group_data'], resp_keys )
+    #     resp_group_data_keys = sorted( resp_dct['response']['group_data'].keys() )
+    #     self.assertEqual( ['count', 'count_estimated', 'date_created', 'date_modified', 'description', 'reference_id', 'uuid' ], resp_group_data_keys )
+    #     ## put -> deletion ======================
+    #     delete_url = reverse( 'data_group_url', kwargs={'incoming_uuid': new_uuid} )
+    #     delete_response = self.client.delete( delete_url )
+    #     self.assertEqual( 200, delete_response.status_code )
+    #     resp_dct = json.loads( delete_response.content )
+    #     self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
+    #     req_keys = sorted( resp_dct['request'].keys() )
+    #     self.assertEqual( ['method', 'payload', 'timestamp', 'url'], req_keys )
+    #     resp_keys = sorted( resp_dct['response'].keys() )
+    #     self.assertEqual( ['elapsed_time', 'status'], resp_keys )
 
     ## DELETE ====================
 
