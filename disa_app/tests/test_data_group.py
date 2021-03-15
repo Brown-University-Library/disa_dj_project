f9d530b1fc2edc7a6b79b6498706b3b867bfc988
birkin <birkin.diana@gmail.com>
2021-03-15 13:03:26 -0400

    updates delete test.

--- disa_app/tests/test_data_group.py
+++ disa_app/tests/test_data_group.py
@@ -21,6 +21,25 @@ TestCase.maxDiff = 1000
 class Client_ReferenceGroup_Test( TestCase ):
     """ Checks reference-group api urls. """
 
+    ## HELPER ====================
+
+    # def create_default_entry(self):
+    #     """ Creates a group for tests below. """
+    #     self.existing_uuid = None
+    #     post_url = reverse( 'data_group_url', kwargs={'incoming_uuid': 'new'} )
+    #     log.debug( f'post-url, ``{post_url}``' )
+    #     payload = {
+    #         'count': 7,
+    #         'count_estimated': True,
+    #         'description': 'the description',
+    #         'reference_id': 49
+    #     }
+    #     jsn = json.dumps( payload )
+    #     response = self.client.post( post_url, data=jsn, content_type='application/json' )
+    #     resp_dct = json.loads( response.content )
+    #     self.existing_uuid = resp_dct['response']['group_data']['uuid']
+    #     log.debug( f'self.existing_uuid, ``{self.existing_uuid}``' )
+
     ## GET =======================
 
     def test_get_bad(self):
@@ -59,29 +78,6 @@ class Client_ReferenceGroup_Test( TestCase ):
         self.assertEqual( 400, response.status_code )
         self.assertTrue( b'Bad Request' in response.content )
 
-    # def test_post_good(self):
-    #     """ Checks `http://127.0.0.1:8000/data/reference_group/abcd/ w/good params. """
-    #     post_url = reverse( 'data_group_url', kwargs={'incoming_uuid': 'new'} )
-    #     log.debug( f'post-url, ``{post_url}``' )
-    #     payload = {
-    #         'count': 7,
-    #         'count_estimated': True,
-    #         'description': 'the description',
-    #         'reference_id': 49
-    #     }
-    #     response = self.client.post( post_url, payload )
-    #     self.assertEqual( 200, response.status_code )
-    #     resp_dct = json.loads( response.content )
-    #     self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
-    #     req_keys = sorted( resp_dct['request'].keys() )
-    #     self.assertEqual( ['method', 'payload', 'timestamp', 'url'], req_keys )
-    #     req_payload_keys = sorted( resp_dct['request']['payload'].keys() )
-    #     self.assertEqual( ['count', 'count_estimated', 'description', 'reference_id'], req_payload_keys )
-    #     resp_keys = sorted( resp_dct['response'].keys() )
-    #     self.assertEqual( ['elapsed_time', 'group_data'], resp_keys )
-    #     resp_group_data_keys = sorted( resp_dct['response']['group_data'].keys() )
-    #     self.assertEqual( ['count', 'count_estimated', 'date_created', 'date_modified', 'description', 'reference_id', 'uuid' ], resp_group_data_keys )
-
     def test_post_good(self):
         """ Checks `http://127.0.0.1:8000/data/reference_group/abcd/ w/good params. """
         post_url = reverse( 'data_group_url', kwargs={'incoming_uuid': 'new'} )
@@ -92,12 +88,9 @@ class Client_ReferenceGroup_Test( TestCase ):
             'description': 'the description',
             'reference_id': 49
         }
-        # response = self.client.post( post_url, payload )
-
         jsn = json.dumps( payload )
-        response = self.client.post( post_url, data=jsn,  content_type='application/json' )
-
-
+        response = self.client.post( post_url, data=jsn, content_type='application/json' )
+        #
         self.assertEqual( 200, response.status_code )
         resp_dct = json.loads( response.content )
         self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
@@ -121,7 +114,7 @@ class Client_ReferenceGroup_Test( TestCase ):
 
     def test_put_good(self):
         """ Checks good PUT of `http://127.0.0.1:8000/data/reference_group/abcd/`. """
-        uuid = 'e2be12a88dc2455dbb0aaaab84828dc8'
+        uuid = '55062fa7c60f4ff9be2698b68ea9da8a'
         self.assertEqual( str, type(uuid) )
         ## put -> put ===========================
         put_url = reverse( 'data_group_url', kwargs={'incoming_uuid': uuid} )
@@ -136,7 +129,9 @@ class Client_ReferenceGroup_Test( TestCase ):
         }
         jsn = json.dumps( put_payload )
         put_response = self.client.put( put_url, data=jsn )
+        # put_response = self.client.put( put_url, data=jsn, content_type='application/json' )
         put_resp_dct = json.loads( put_response.content )
+        #
         self.assertEqual( 200, put_response.status_code )
         resp_dct = json.loads( put_response.content )
         self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
@@ -148,55 +143,6 @@ class Client_ReferenceGroup_Test( TestCase ):
         self.assertEqual( ['elapsed_time', 'group_data'], resp_keys )
         resp_group_data_keys = sorted( resp_dct['response']['group_data'].keys() )
         self.assertEqual( ['count', 'count_estimated', 'date_created', 'date_modified', 'description', 'reference_id', 'uuid' ], resp_group_data_keys )
-
-    # def test_put_good(self):
-    #     """ Checks good PUT of `http://127.0.0.1:8000/data/reference_group/abcd/`. """
-    #     ## put -> create entry ==================
-    #     post_url = reverse( 'data_group_url', kwargs={'incoming_uuid': 'new'} )
-    #     log.debug( f'post-url, ``{post_url}``' )
-    #     post_payload = {
-    #         'count': 7,
-    #         'count_estimated': True,
-    #         'description': 'the description',
-    #         'reference_id': 49
-    #     }
-    #     post_response = self.client.post( post_url, post_payload )
-    #     post_resp_dct = json.loads( post_response.content )
-    #     new_uuid = post_resp_dct['response']['group_data']['uuid']
-    #     self.assertEqual( str, type(new_uuid) )
-    #     ## put -> put ===========================
-    #     put_url = reverse( 'data_group_url', kwargs={'incoming_uuid': new_uuid} )
-    #     log.debug( f'put_url-url, ``{put_url}``' )
-    #     put_payload = {
-    #         'count': 8,
-    #         'count_estimated': False,
-    #         'description': 'the new description',
-    #         'reference_id': 49
-    #     }
-    #     jsn = json.dumps( put_payload )
-    #     put_response = self.client.put( put_url, data=jsn )
-    #     put_resp_dct = json.loads( put_response.content )
-    #     self.assertEqual( 200, put_response.status_code )
-    #     resp_dct = json.loads( put_response.content )
-    #     self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
-    #     req_keys = sorted( resp_dct['request'].keys() )
-    #     self.assertEqual( ['method', 'payload', 'timestamp', 'url'], req_keys )
-    #     req_payload_keys = sorted( resp_dct['request']['payload'].keys() )
-    #     self.assertEqual( ['count', 'count_estimated', 'description', 'reference_id'], req_payload_keys )
-    #     resp_keys = sorted( resp_dct['response'].keys() )
-    #     self.assertEqual( ['elapsed_time', 'group_data'], resp_keys )
-    #     resp_group_data_keys = sorted( resp_dct['response']['group_data'].keys() )
-    #     self.assertEqual( ['count', 'count_estimated', 'date_created', 'date_modified', 'description', 'reference_id', 'uuid' ], resp_group_data_keys )
-    #     ## put -> deletion ======================
-    #     delete_url = reverse( 'data_group_url', kwargs={'incoming_uuid': new_uuid} )
-    #     delete_response = self.client.delete( delete_url )
-    #     self.assertEqual( 200, delete_response.status_code )
-    #     resp_dct = json.loads( delete_response.content )
-    #     self.assertEqual( ['request', 'response'], sorted(resp_dct.keys()) )
-    #     req_keys = sorted( resp_dct['request'].keys() )
-    #     self.assertEqual( ['method', 'payload', 'timestamp', 'url'], req_keys )
-    #     resp_keys = sorted( resp_dct['response'].keys() )
-    #     self.assertEqual( ['elapsed_time', 'status'], resp_keys )
 
     ## DELETE ====================
 
@@ -218,7 +164,8 @@ class Client_ReferenceGroup_Test( TestCase ):
             'description': 'the description',
             'reference_id': 49
         }
-        post_response = self.client.post( post_url, payload )
+        jsn = json.dumps( payload )
+        post_response = self.client.post( post_url, data=jsn, content_type='application/json' )
         post_resp_dct = json.loads( post_response.content )
         new_uuid = post_resp_dct['response']['group_data']['uuid']
         self.assertEqual( str, type(new_uuid) )
