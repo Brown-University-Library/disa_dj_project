836b48e9898f691a986ac78e63e6cf979f1945ef
Patrick Rashleigh <edward_rashleigh@brown.edu>
2021-05-05 14:58:40 -0400

    bottom buttons now work!

--- disa_app/static/js/redesign_citation.js
+++ disa_app/static/js/redesign_citation.js
@@ -69,6 +69,9 @@ function initializeCitationForm(dataAndSettings) {
       'formData.doc.citation_type_id': updateCitationFieldVisibility
     },
     methods: {
+      openItemTab: function () {
+        document.getElementById('item-tab').click();
+      },
       onSubmitForm: function(x) { 
         // @todo finish this
         console.log({ 
