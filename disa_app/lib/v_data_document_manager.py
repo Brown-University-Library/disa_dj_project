74bc4554c51d5997d911fcfc46d3fb201a00bdcc
birkin <birkin.diana@gmail.com>
2021-05-06 12:04:16 -0400

    restores citation tests.

--- disa_app/lib/v_data_document_manager.py
+++ disa_app/lib/v_data_document_manager.py
@@ -37,6 +37,7 @@ def make_session() -> sqlalchemy.orm.session.Session:
 def manage_get( doc_id: str, user_id: int ) -> dict:
     """ Queries and massages data for given doc_id.
         Called by views.data_documents() on GET """
+    assert type(doc_id) == str; assert type(user_id) == int
     session = make_session()
     data = { 'doc': {} }
     included: list = CITATION_TYPES
@@ -48,6 +49,7 @@ def manage_get( doc_id: str, user_id: int ) -> dict:
     # if doc_id == None:
     #     log.debug( f'returning data for docID equals None, ```{pprint.pformat(data)}```' )
     #     return jsonify(data)
+
     if doc_id == 'copy':
 
         last_edit = edit = session.query( models_alch.ReferenceEdit ).filter_by( user_id=user_id ).order_by( models_alch.ReferenceEdit.timestamp.desc() ).first()
@@ -60,6 +62,9 @@ def manage_get( doc_id: str, user_id: int ) -> dict:
         log.debug( f'doc, ```{doc}```' )
     else:
         doc = session.query( models_alch.Citation ).get( doc_id )
+        if doc == None:
+            log.debug( 'citation query not found' )
+            return 'not_found'
         data['doc']['id'] = doc.id
     data['doc']['citation'] = doc.display
     # data['doc']['zotero_id'] = doc.zotero_id
@@ -80,7 +85,8 @@ def manage_get( doc_id: str, user_id: int ) -> dict:
 
 def manage_get_all( user_id: int ) -> dict:
     """ Queries and massages data for new-document.
-        Called by views.data_documents() on GET, with no doc_id """
+        Called by views.data_documents() on GET, with no doc_id.
+        2021-April-27: i don't think this is being used. """
     log.debug( f'\n\nstarting manage_get_all()' )
     session = make_session()
     data = { 'doc': {} }
@@ -96,8 +102,16 @@ def manage_get_all( user_id: int ) -> dict:
 def manage_put( cite_id: str, user_id: int, payload: bytes ):
     """ Updates document.
         Called by views.data_documents() on PUT """
+    assert type(cite_id) == str; assert type(user_id) == int; assert type(payload) == bytes
     session = make_session()
-    data: dict = json.loads( payload )
+
+    try:
+        data: dict = json.loads( payload )
+        log.debug( f'data (from payload), ``{pprint.pformat(data)}``' )
+    except:
+        log.exception( 'problem parsing payload; error will be returned' )
+        return '400 / Bad Request'
+
     try:  # temp; remove after debugging
         unspec = session.query( models_alch.CitationType ).filter_by( name='Document' ).first()
 
@@ -154,6 +168,7 @@ def manage_put( cite_id: str, user_id: int, payload: bytes ):
 
     except:
         log.exception( 'problem on api PUT; traceback follows...' )
+    log.debug( f'data (put-context), ``{pprint.pformat(data)}``' )
     return data
 
     ## end def manage_put()
@@ -162,24 +177,33 @@ def manage_put( cite_id: str, user_id: int, payload: bytes ):
 def manage_post( user_id, payload ):
     """ Updates document.
         Called by views.data_documents() on POST """
-    session = make_session()
-    data: dict = json.loads( payload )
-    try:  # temp; remove after debugging
-        unspec = session.query( models_alch.CitationType ).filter_by( name='Document' ).first()
+    log.debug( 'starting manage_post()' )
+    assert type(user_id) == int; assert type(payload) == bytes
+    log.debug( f'user_id, ``{user_id}``' )
+    log.debug( f'payload, ``{payload}``' )
 
+    try:  # temp; remove after debugging
+
+        session = make_session()
+        data: dict = json.loads( payload )
+        log.debug( f'data, ``{pprint.pformat(data)}``' )
+
+        log.debug( f'initial data["citation_type_id"], ``{data["citation_type_id"]}``' )
+
+        unspec = session.query( models_alch.CitationType ).filter_by( name='Document' ).first()
         data['citation_type_id'] = data['citation_type_id'] or unspec.id
 
-        # cite = models.Citation(citation_type_id=data['citation_type_id'],
-        #     comments=data['comments'], acknowledgements=data['acknowledgements'])
+        log.debug( f'unspec.id, ``{unspec.id}``')
+        log.debug( f'final data["citation_type_id"], ``{data["citation_type_id"]}``' )
 
         cite = models_alch.Citation(
             citation_type_id=data['citation_type_id'],
             comments=data['comments'],
             acknowledgements=data['acknowledgements']
             )
-
-        session.add(cite)
+        session.add(cite)  # this creates the citation in mysql
         session.commit()
+        log.debug( 'citation-obj added to mysql' )
 
         field_order_map = { f.zotero_field.name: f.rank
             for f in cite.citation_type.zotero_type.template_fields }
@@ -201,21 +225,24 @@ def manage_post( user_id, payload ):
 
             citation_display.append( (field_order_map[zfield.name], val) )
             session.add(cfield)
+
+        log.debug( f'initial citation_display, ``{citation_display}``' )
         if len(citation_display) == 0:
             now = datetime.datetime.utcnow()
             cite.display = 'Document :: {}'.format(now.strftime('%Y %B %d'))
         else:
             vals = [ v[1] for v in sorted(citation_display) ]
             cite.display = ' '.join(vals)
+        log.debug( f'final citation_display, ``{citation_display}``' )
 
-        session.add(cite)
+        session.add(cite)  # I assume this simply updates the existing citation in mysql
         session.commit()
 
         context = { 'redirect': reverse( 'edit_citation_url', kwargs={'cite_id': cite.id} ) }
 
     except:
-        log.exception( 'problem on api POST; traceback follows...' )
-        context = {}
+        log.exception( 'problem on api POST; traceback follows; processing will continue.' )
+        context = 'error'
 
     log.debug( f'context, ```{context}```' )
     return context
@@ -227,40 +254,50 @@ def manage_delete( doc_id, user_uuid_str, user_email ):
     """ Adds mark-for-deletion entry to django-db table.
         Called by: views.data_documents() when request.method is 'DELETE'. """
     log.debug( f'doc_id, ``{doc_id}``; user_uuid_str, ``{user_uuid_str}``; user_email, ``{user_email}``' )
-    log.debug( f'type(user_uuid_str), ``{type(user_uuid_str)}``' )
-    session = make_session()
-    log.debug( f'session, ``{session}``' )
-    citation_result = session.query( models_alch.Citation ).get( doc_id )
-    log.debug( f'citation_result, ``{citation_result}``' )
+    assert type(doc_id) == str
+    assert type(user_uuid_str) == str
+    assert type(user_email) == str
+    context = None
 
-    citaton_dct = citation_result.dictify()
-    log.debug( f'citaton_dct, ``{citaton_dct}``' )
-
+    ## build citation_jsn for storage
     try:
+        session = make_session()
+        log.debug( f'session, ``{session}``' )
+        citation_result = session.query( models_alch.Citation ).get( doc_id )
+        log.debug( f'citation_result, ``{citation_result}``' )
+        citaton_dct = citation_result.dictify()
+        log.debug( f'citaton_dct, ``{citaton_dct}``' )
         citation_jsn = json.dumps( citaton_dct, sort_keys=True, indent=2 )
         log.debug( f'citation_jsn, ``{citation_jsn}``' )
     except:
-        log.exception( 'problem getting citation json' )
+        log.exception( 'problem getting citation json; traceback follows; returning error-message' )
+        context = '500 / Server Error'
 
-    try:
-        usr_dct = { 'user_uuid': user_uuid_str, 'user_email': user_email }
-        user_jsn = json.dumps( usr_dct, sort_keys=True, indent=2 )
-    except:
-        log.exception( 'problem getting user json' )
+    ## build user_jsn for storage
+    if context == None:
+        try:
+            usr_dct = { 'user_uuid': user_uuid_str, 'user_email': user_email }
+            user_jsn = json.dumps( usr_dct, sort_keys=True, indent=2 )
+        except:
+            log.exception( 'problem getting user json; traceback follows; returning error-message' )
+            context = '500 / Server Error'
 
-    try:
-        log.debug( 'saving markedfordeletion_entry' )
-        markedfordeletion_entry = MarkedForDeletion(
-            old_db_id=doc_id, doc_json_data=citation_jsn, patron_json_data=user_jsn )
-        markedfordeletion_entry.save()
-        log.debug( 'markedfordeletion_entry seems to have worked' )
-    except:
-        log.exception( 'problem saving markedfordeletion_entry' )
-    # context = { 'status': 'foo' }
-    # log.debug( f'context, ``{context}``' )
-    # return context
-    return
-
+    ## save the citation_jsn and user_jsn to the marked-for-deletion entry
+    if context == None:
+        try:
+            log.debug( 'saving markedfordeletion_entry' )
+            markedfordeletion_entry = MarkedForDeletion(
+                old_db_id=doc_id,
+                doc_json_data=citation_jsn,
+                patron_json_data=user_jsn )
+            markedfordeletion_entry.save()
+            context = { 'marked_for_deletion_result': 'success' }
+            log.debug( 'markedfordeletion_entry save successful' )
+        except:
+            log.exception( 'problem saving markedfordeletion_entry; traceback follows; eturning error-message' )
+            context = '500 / Server Error'
+    log.debug( f'context, ``{context}``' )
+    return context
 
 # def manage_delete( doc_id, user_id ):
 #     """ Adds mark-for-deletion entry to django-db table.
