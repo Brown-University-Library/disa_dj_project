f4b5104dc43ecdc132e8aae4f6054f124128c07d
Patrick Rashleigh <edward_rashleigh@brown.edu>
2021-05-21 13:46:54 -0400

    adds data submission routine for Items/Records

--- disa_app/static/js/entry_form_vue-item_mixin_save.js
+++ disa_app/static/js/entry_form_vue-item_mixin_save.js
@@ -2,7 +2,7 @@
 // This module collects all the functionality related to saving to the server
 
 
-async function saveReferentData(referentId, itemId, apiDefinition, requestBody) {
+async function saveReferentData_post(referentId, itemId, apiDefinition, requestBody) {
 
   const fetchOptions = {
           method: apiDefinition.api_method,
@@ -27,21 +27,29 @@ async function saveReferentData(referentId, itemId, apiDefinition, requestBody) 
 };
 
 
-function submitReferentDataToServer() {
+function saveReferentDataToServer() {
 
   console.log('CALLING ROUTINE TO SUBMIT REFERENT DATA');
 
   if (this.currentReferent.FULL_DATA_LOADED) {
 
+    // Convert from Tagify to DB-ready data structure
+
+    function convertFromTagify(tagifyString) {
+      return JSON.parse(tagifyString || '[]').map(
+        tagData => { return { id: tagData.dbID, name: tagData.value } }
+      );
+    }
+
     // Map data in Vue to what's expected by the API
 
     const requestBody = JSON.stringify({
-      id: this.currentReferent.id,
       age: this.currentReferent.age,
-      // name: this.currentReferent.names[0], // @todo - only one name?? BD's test has a .name field
-      names: this.currentReferent.names,
+      names: this.currentReferent.names.map(
+        name => Object.assign({}, name, { id: name.id.toString() })
+      ),
       origins: this.currentReferent.origins,
-      races: this.currentReferent.races,
+      races: convertFromTagify(this.currentReferent.races),
       sex: this.currentReferent.sex,
       statuses: [], // this.currentReferent.statuses, // ??
       titles: this.currentReferent.titles.map(
@@ -50,35 +58,32 @@ function submitReferentDataToServer() {
           return title 
         }
       ),
-      tribes: this.currentReferent.tribes,
-      vocations: this.currentReferent.vocations
+      tribes: convertFromTagify(this.currentReferent.tribes),
+      vocations: convertFromTagify(this.currentReferent.vocations)
     });
 
     console.log('SAVE CURRENT REFERENT');
     this.saveStatus = this.SAVE_STATUS.SAVE_IN_PROGRESS;
 
-    saveReferentData(
+    saveReferentData_post(
       this.currentReferentId, 
       this.currentItemId,
       this.saveCurrentReferentAPI,
       requestBody
-    );
-
-    window.setTimeout( // FAKE FETCH
-      () => { 
-        this.saveStatus = this.SAVE_STATUS.SUCCESS; 
-        console.log('SAVED!'); 
-      }, 
-      2000
+    ).then(
+      () => { this.saveStatus = this.SAVE_STATUS.SUCCESS }
     );
   }
 }
 
-function submitItemDataToServer() {
+
+async function saveItemDataToServer() {
 
   console.log('CALLING ROUTINE TO SUBMIT ITEM DATA');
+  console.log(' FULL DATA LOADED? ' + this.currentItem.FULL_DATA_LOADED);
 
   if (this.currentItem.FULL_DATA_LOADED) {
+
     this.saveStatus = this.SAVE_STATUS.SAVE_IN_PROGRESS;
 
     // Only save current Item (without Referent data)
@@ -86,19 +91,89 @@ function submitItemDataToServer() {
     // const { referents, ...currentItemDataNoReferents } = this.currentItem;
     // const currentItemCopy = JSON.parse(JSON.stringify(currentItemDataNoReferents));
 
-    // @todo INSTEAD ... create a completely new object as API payload
+    const locations = [],
+          makeLocationObj = locationType => {
+            return {
+              id: this.currentItem.location_info[locationType].id || -1,
+              label: this.currentItem.location_info[locationType].name,
+              value: this.currentItem.location_info[locationType].name
+            }
+          };
 
-    const submitPayload = {
-      date: ['day','month','year'].map(x => this.currentItem.dateParts[x]).join('/')
-      // ... and so on
+    if (this.currentItem.location_info['Colony/State']) {
+      locations.push(makeLocationObj('Colony/State'));
+      if (this.currentItem.location_info['City']) {
+        locations.push(makeLocationObj('City'));
+        if (this.currentItem.location_info['Locale']) {
+          locations.push(makeLocationObj('Locale'));
+        }
+      }
+    }
+/*
+    const locations = ['Colony/State','City','Locale'].filter(
+      locationType => (this.currentItem.location_info[locationType].name)
+    ).map(locationType => {
+      return {
+        id: this.currentItem.location_info[locationType].id || -1,
+        label: this.currentItem.location_info[locationType].name,
+        value: this.currentItem.location_info[locationType].name
+      }
+    }); */
+
+    let date;
+
+    if (this.currentItem.dateParts.year) {
+      const month = this.currentItem.dateParts.month !== -1
+          ? this.currentItem.dateParts.month
+          : '01',
+        day = this.currentItem.dateParts.day
+          ? this.currentItem.dateParts.day.toString().padStart(2, '0')
+          : '01',
+        year = this.currentItem.dateParts.year;
+      date = `${month}/${day}/${year}`;
+    } else {
+      date = null;
     }
 
-    console.log('SAVING ITEM DATA ...', submitPayload);
+    const requestBody = {
+      locations,
+      date,
+      transcription: this.currentItem.transcription,
+      record_type: {
+         id: this.currentItem.reference_type_id
+         //"value":"Runaway",
+         //"label":"Runaway" @todo Q: ARE THESE NECESSARY?
+      },
+      national_context: this.currentItem.national_context_id,
+      citation_id: this.formData.doc.id,
+      image_url: this.currentItem.image_url
+    };
 
-    window.setTimeout( // FAKE FETCH
-      () => this.saveStatus = this.SAVE_STATUS.SUCCESS, 
-      2000
-    );
+    const url = `http://127.0.0.1:8000/data/records/${this.currentItemId}/`,
+          fetchOptions = {
+            method: 'PUT', // apiDefinition.api_method,
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRFToken': TOKEN
+            },
+            body: JSON.stringify(requestBody)
+          };
+
+    console.log(`SAVE ITEM FETCH OPTIONS - putting to ${url}`, fetchOptions);
+
+    if (false) { return }  // Skip send if true
+
+    this.saveStatus = this.SAVE_STATUS.SAVE_IN_PROGRESS;
+    const response = await fetch(url, fetchOptions);
+    if (response.ok) {
+      const dataJSON = await response.json();
+      // this.currentItem.relationships = dataJSON.store;
+      this.saveStatus = this.SAVE_STATUS.SUCCESS;
+      console.log('RESPONSE', {response, dataJSON}) ;
+    } else {
+      this.saveStatus = this.SAVE_STATUS.ERROR;
+      throw Error(response.statusText);
+    }
   }
 }
 
@@ -140,6 +215,37 @@ async function createRelationshipOnServer() {
 }
 
 
+async function deleteRelationshipOnServer(relationship) {
+  console.log(`DELETING RELATIONSHIP ID: ${relationship.id}`);
+  const url = `http://127.0.0.1:8000/data/relationships/${relationship.id}`,
+        requestBody = { section: this.currentItemId },
+        fetchOptions = {
+          method: 'DELETE', // apiDefinition.api_method,
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': TOKEN
+          },
+          body: JSON.stringify(requestBody)
+        };
+  
+  this.saveStatus = this.SAVE_STATUS.SAVE_IN_PROGRESS;
+  const response = await fetch(url, fetchOptions);
+
+  if (response.ok) {
+    const dataJSON = await response.json();
+    this.currentItem.relationships = dataJSON.store;
+    this.saveStatus = this.SAVE_STATUS.SUCCESS;  
+  } else {
+    this.saveStatus = this.SAVE_STATUS.ERROR;
+    throw Error(response.statusText);
+  }
+  
+  // @todo - also delete inverse relationship?
+}
+
+
+// Vue Mixin
+
 const saveFunctionsMixin = {
 
   computed: {
@@ -177,7 +283,7 @@ const saveFunctionsMixin = {
 
   methods: {
     createRelationshipOnServer,
-    deleteRelationshipOnServer,
+    deleteRelationshipOnServer
   }
 }
 
