c0f178ed386795ab87716cdb87a326346197bd40
Patrick Rashleigh <edward_rashleigh@brown.edu>
2021-09-09 12:30:55 -0400

    adds a "loading ..." popup message to Experimental Browse while JSON is downloading

--- disa_app/static/js/browse_tabulator_getData.js
+++ disa_app/static/js/browse_tabulator_getData.js
@@ -2,16 +2,28 @@
 // Loads the SR data
 
 async function getStolenRelationsData(url) {
+
+  const loadingModalElem = document.getElementById('loadingModal'),
+        loadingModal = new bootstrap.Modal(loadingModalElem);
+
+  loadingModal.show();
+
   const data = await fetch(url).then(response => {
     if (!response.ok) {
-      throw new Error("Couldn't load Stolen Relations data");
+      throw new Error(`Status ${response.status} (${response.statusText})`);
     } else {
+      // Not sure why, but Bootstrap requires a delay here ...
+      window.setTimeout(() => loadingModal.hide(), 1000);
       return response.json();
     }
   })
   .catch(error => {
+    document.getElementById('loadingModalMessage').innerHTML = `
+      Couldn't load Stolen Relations data<br />${error}
+    `;
     console.error("Couldn't load Stolen Relations data", error);
   });
+
   return data;
 }
 
