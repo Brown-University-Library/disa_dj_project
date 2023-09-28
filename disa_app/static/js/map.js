ea62ee25296d8291ccd9ea0c4e6b2e142ba60f04
Elizabeth Yalkut <elizabeth_yalkut@brown.edu>
2023-09-28 15:06:29 -0400

    getting the static map data to work on remote servers, removing test code

--- disa_app/static/js/map.js
+++ disa_app/static/js/map.js
@@ -60,7 +60,7 @@ basemaps.Terrain.addTo(map);
 
 // fetch the geojson
 var geoJsonData = new L.GeoJSON.AJAX(
-    "/static/data/SR_geo.json", {
+    leaflet_data_url, {
 
         // build each point
         onEachFeature: function(feature, layer) {
